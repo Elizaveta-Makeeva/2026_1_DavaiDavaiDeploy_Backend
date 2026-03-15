7bd589590ee6ee25c1fbb4b9b7504033fb68a278
Elizaveta-Makeeva <makeevaea2020@yandex.ru>
2026-03-15 23:44:36 +0300

    [ddd-5][auth] add main

---
+++ cmd/auth/main.go
@@ -0,0 +1,104 @@
+// @title           DDDance API
+// @version         1.0
+// @description     API для авторизации пользователей
+// @host            localhost:5458
+// @BasePath        /api
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"log/slog"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gorilla/mux"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
+
+	authHandler "DDDance/internal/pkg/auth/delivery/grpc"
+	authRepo "DDDance/internal/pkg/auth/repo"
+	authUsecase "DDDance/internal/pkg/auth/usecase"
+	"DDDance/internal/pkg/middleware/logger"
+
+	"DDDance/internal/pkg/auth/delivery/grpc/gen"
+)
+
+func initDB(ctx context.Context) (*pgxpool.Pool, error) {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASS")
+	dbname := os.Getenv("DB_NAME")
+
+	postgresString := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname,
+	)
+
+	config, err := pgxpool.ParseConfig(postgresString)
+	if err != nil {
+		return nil, err
+	}
+
+	pool, err := pgxpool.ConnectConfig(ctx, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return pool, nil
+}
+
+func main() {
+	_ = godotenv.Load()
+	ctx := context.Background()
+
+	dbpool, err := initDB(ctx)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+	defer dbpool.Close()
+
+	authRepo := authRepo.NewAuthRepository(dbpool)
+	authUsecase := authUsecase.NewAuthUsecase(authRepo)
+
+	// инициализация gRPC хендлера
+	authHandler := authHandler.NewGrpcAuthHandler(authUsecase)
+
+	ddLogger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	gRPCServer := grpc.NewServer(grpc.ChainUnaryInterceptor(logger.LoggerInterceptor(ddLogger)))
+	gen.RegisterAuthServer(gRPCServer, authHandler)
+
+	r := mux.NewRouter().PathPrefix("").Subrouter()
+	http.Handle("/", r)
+	httpSrv := http.Server{Handler: r, Addr: ":5460"}
+	//запуск мониторинга
+	go func() {
+		if err := httpSrv.ListenAndServe(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	go func() {
+		listener, err := net.Listen("tcp", fmt.Sprintf(":%d", 5459))
+		if err != nil {
+			fmt.Println(err)
+		}
+		if err := gRPCServer.Serve(listener); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+	<-stop
+
+	log.Println("Shutting down auth gRPC server...")
+	gRPCServer.GracefulStop()
+	log.Println("Auth server exited")
+}
--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,31 +1,113 @@
+// @title           Kinopoisk API
+// @version         1.0
+// @description     API для авторизации пользователей и получения фильмов/жанров/актеров.
+// @host            localhost:5458
+// @BasePath        /api
 package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
-	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
+	authHandlers "DDDance/internal/pkg/auth/delivery/http"
+	authRepo "DDDance/internal/pkg/auth/repo"
+	authUsecase "DDDance/internal/pkg/auth/usecase"
+	"DDDance/internal/pkg/middleware/cors"
+	logger "DDDance/internal/pkg/middleware/logger"
+	"os"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+	"github.com/joho/godotenv"
+	"google.golang.org/grpc"
+
 	"github.com/gorilla/mux"
+
+	authGen "DDDance/internal/pkg/auth/delivery/grpc/gen"
 )
 
+func initDB(ctx context.Context) (*pgxpool.Pool, error) {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASS")
+	dbname := os.Getenv("DB_NAME")
+
+	postgresString := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname,
+	)
+
+	config, err := pgxpool.ParseConfig(postgresString)
+	if err != nil {
+		return nil, err
+	}
+
+	pool, err := pgxpool.ConnectConfig(ctx, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return pool, nil
+}
+
 func main() {
-	r := mux.NewRouter().PathPrefix("/api").Subrouter()
-	r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	_ = godotenv.Load()
+	ctx := context.Background()
+	dbpool, err := initDB(ctx)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+	defer dbpool.Close()
+
+	// Подключение к auth microservice
+	authConn, err := grpc.Dial("auth:5459", grpc.WithInsecure())
+	if err != nil {
+		log.Printf("unable to connect to auth microservice: %v\n", err)
+		return
+	}
+	defer authConn.Close()
+
+	authClient := authGen.NewAuthClient(authConn)
+	authRepo := authRepo.NewAuthRepository(dbpool)
+	authUsecase := authUsecase.NewAuthUsecase(authRepo)
+
+	authHandler := authHandlers.NewAuthHandler(authClient, authUsecase)
+
+	ddLogger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	mainRouter := mux.NewRouter()
+
+	apiRouter := mainRouter.PathPrefix("/api").Subrouter()
+	apiRouter.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "I am not giving any dances!", http.StatusTeapot)
 	})
-	http.Handle("/", r)
+
+	apiRouter.Use(cors.CorsMiddleware)
+	apiRouter.Use(logger.LoggerMiddleware(ddLogger))
+
+	// Auth routes
+	authRouter := apiRouter.PathPrefix("/auth").Subrouter()
+	authRouter.HandleFunc("/signup", authHandler.SignupUser).Methods(http.MethodPost, http.MethodOptions)
+	authRouter.HandleFunc("/signin", authHandler.SignInUser).Methods(http.MethodPost, http.MethodOptions)
+
+	protectedAuthRouter := authRouter.PathPrefix("").Subrouter()
+	protectedAuthRouter.Use(authHandler.Middleware)
+	protectedAuthRouter.HandleFunc("/check", authHandler.CheckAuth).Methods(http.MethodGet, http.MethodOptions)
+	protectedAuthRouter.HandleFunc("/logout", authHandler.LogOutUser).Methods(http.MethodPost, http.MethodOptions)
 
 	danceSrv := http.Server{
-		Handler: r,
+		Handler: mainRouter,
 		Addr:    ":5458",
 	}
 
 	go func() {
-		log.Println("Starting server!")
+		log.Println("Starting main server on port 5458!")
 		err := danceSrv.ListenAndServe()
 		if err != nil && err != http.ErrServerClosed {
 			log.Printf("Server start error: %v", err)
@@ -41,11 +123,10 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
-	err := danceSrv.Shutdown(ctx)
+	err = danceSrv.Shutdown(ctx)
 	if err != nil {
 		log.Printf("Graceful shutdown failed")
 		os.Exit(1)
 	}
-	log.Printf("Graceful shutdown!")
-	os.Exit(0)
+	log.Printf("Graceful shutdown!!")
 }
